fe89c6cd01b1a5f0332a6748ad7ef7322a31edb6
gAldeia <guilherme.seidyo@gmail.com>
2026-05-01 13:37:19 -0400

    Fixing numpy version to most compatible across methods

--- algorithms/e2et/install.sh
+++ algorithms/e2et/install.sh
@@ -2,8 +2,14 @@
 #git clone https://github.com/pakamienny/sympytorch.git
 pip install git+https://github.com/pakamienny/sympytorch.git@rationals
 
-
-#wget -nc https://dl.fbaipublicfiles.com/symbolicregression/model1.pt
+# Downloading pretrained model
+# > wget -nc https://dl.fbaipublicfiles.com/symbolicregression/model1.pt
+# OR
+# > cd /
+# > mkdir pretrained
+# > curl https://dl.fbaipublicfiles.com/symbolicregression/model1.pt --output /pretrained/model.pt
+# Save it locally and keep it in an accessible folder, instead of inside the 
+# docker image. You need to specify the path to the pretrained models
 
 pip install git+https://github.com/pakamienny/e2e_transformer.git
 
