37a4ebe7d4f954b203abd29a4ddb6cec253e7986
gAldeia <guilherme.seidyo@gmail.com>
2026-05-05 11:51:39 -0400

    Fixing ellyn instalation issues

--- algorithms/afp/install.sh
+++ algorithms/afp/install.sh
@@ -7,4 +7,7 @@ cd ellyn
 # fix version
 git checkout cdff25b2851d942db1cdb2a6796ea61c41396c7c
 
+# Ellyn depends on deprecated Boost timer headers on recent Boost releases.
+sed -i '1i #define BOOST_TIMER_ENABLE_DEPRECATED\n#define BOOST_ERROR_CODE_HEADER_ONLY' src/ellen/stdafx.h
+
 python setup.py install
